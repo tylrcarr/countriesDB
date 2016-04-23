fbe150b0f87826b6a3b773624d27003aedfba023
tylrcarr <tylrcarr@gmail.com>
2016-04-22 20:43:40 -0400

    save

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/admin', function(req, res) {
-  res.render('admin', { title: 'Troy is my one true love' });
+  res.render('admin', { title: 'Bananas are the best fruit' });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Troy is my one true love' });
+  res.render('index', { title: 'Countries Comparer' });
 });
 
 module.exports = router;
